20a6b7667685f9159b60e2fc44cc4617c717a170
Justin Emgarten <justin@emgarten.com>
2017-01-02 21:46:21 -0800

    Adding build scripts

--- test/NuGet.Test.Helpers.Tests/NupkgTests.cs
+++ test/NuGet.Test.Helpers.Tests/NupkgTests.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Linq;
 using FluentAssertions;
 using NuGet.Packaging;
 using Xunit;
@@ -42,6 +40,44 @@ namespace NuGet.Test.Helpers.Tests
 
                 // Verify
                 reader.NuspecReader.GetVersion().ToFullString().Should().Be("2.0.1-rc.5.10+hash.11");
+            }
+        }
+
+        [Fact]
+        public void VerifyNupkgPackageVersionIsNonNormalized()
+        {
+            using (var folder = new TestFolder())
+            {
+                // Arrange
+                var nupkg = TestNupkg.Create("packageA", "1.0");
+
+                // Act
+                var path = nupkg.Save(folder);
+
+                var reader = new PackageArchiveReader(path.FullName);
+
+                // Verify
+                reader.NuspecReader.GetVersion().ToString().Should().Be("1.0");
+            }
+        }
+
+        [Fact]
+        public void VerifyNupkgHasAddedLibFile()
+        {
+            using (var folder = new TestFolder())
+            {
+                // Arrange
+                var nupkg = TestNupkg.Create("packageA", "1.0.0");
+                nupkg.AddFile("lib/net45/a.dll");
+
+                // Act
+                var path = nupkg.Save(folder);
+                var reader = new PackageArchiveReader(path.FullName);
+
+                var dll = reader.GetLibItems().Single().Items.Single();
+
+                // Verify
+                dll.Should().Be("lib/net45/a.dll");
             }
         }
     }
