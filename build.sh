4a59088fa8a7f250090285d35598506b5f07d888
Justin Emgarten <justin@emgarten.com>
2017-01-28 19:29:31 -0800

    Updating build scripts for CLI rc4

--- build.sh
+++ build.sh
@@ -2,9 +2,6 @@
 
 RESULTCODE=0
 
-# increase open file limit for osx
-ulimit -n 2048
-
 # Download dotnet cli
 echo "Installing dotnet"
 mkdir -p .cli
@@ -12,20 +9,56 @@ curl -o .cli/dotnet-install.sh https://raw.githubusercontent.com/dotnet/cli/58b0
 
 # Run install.sh
 chmod +x .cli/dotnet-install.sh
-.cli/dotnet-install.sh -i .cli -c preview -v 1.0.0-preview4-004233
+.cli/dotnet-install.sh -i .cli -c preview -v 1.0.0-rc4-004706
 
 # Display info
 DOTNET="$(pwd)/.cli/dotnet"
 $DOTNET --info
 
-# restore
-$DOTNET restore NuGet.Test.Helpers.sln
+# clean up
+rm -rf $(pwd)/artifacts
+mkdir $(pwd)/artifacts
 
-$DOTNET test test/NuGet.Test.Helpers.Tests/NuGet.Test.Helpers.Tests.csproj -f netcoreapp1.0
+# restore
+$DOTNET restore $(pwd)
 
 if [ $? -ne 0 ]; then
-    echo "$testProj FAILED"
-    RESULTCODE=1
+    echo "Restore FAILED!"
+    exit 1
+fi
+
+# build
+$DOTNET build $(pwd) -c release
+
+if [ $? -ne 0 ]; then
+    echo "Build FAILED!"
+    exit 1
+fi
+
+# run all test projects under test/
+for testProject in `find test -type f -name *.csproj`
+do
+	testDir="$(pwd)/$(dirname $testProject)"
+
+	$DOTNET test testDir -f netcoreapp1.0 --no-build -r $(pwd)/artifacts
+
+	if [ $? -ne 0 ]; then
+	    echo "$testProject FAILED!"
+	    RESULTCODE=1
+	fi
+done
+
+if [ $RESULTCODE -ne 0 ]; then
+    echo "tests FAILED!"
+    exit 1
+fi
+
+# pack
+$DOTNET pack --no-build -o $(pwd)/artifacts
+
+if [ $RESULTCODE -ne 0 ]; then
+    echo "pack FAILED!"
+    exit 1
 fi
 
 exit $RESULTCODE
