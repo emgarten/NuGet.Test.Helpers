d4e85109c346079288401dac3bde80874240f89c
Justin Emgarten <justin@emgarten.com>
2017-10-12 23:57:46 -0700

    Updating CLI to 2.0.2

--- build/common/common.ps1
+++ build/common/common.ps1
@@ -22,7 +22,7 @@ Function Install-DotnetCLI {
 
         wget https://raw.githubusercontent.com/dotnet/cli/c497bf498fd4e964b00e2ee44bd840f2a269ea6c/scripts/obtain/dotnet-install.ps1 -OutFile $installDotnet
 
-        & $installDotnet -Channel 2.0 -i $CLIRoot -Version 2.0.0
+        & $installDotnet -Channel 2.0 -i $CLIRoot -Version 2.0.2
 
         if (-not (Test-Path $DotnetExe)) {
             Write-Log "Missing $DotnetExe"
