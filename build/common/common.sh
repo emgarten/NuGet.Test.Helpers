6dc2c27f8528d787ee725f9dca0c7061b2e1d6c4
Justin Emgarten <justin@emgarten.com>
2024-01-13 20:03:25 +0000

    add net8

--- build/common/common.sh
+++ build/common/common.sh
@@ -19,6 +19,7 @@ run_standard_tests()
     chmod +x .cli/dotnet-install.sh
     .cli/dotnet-install.sh -i .cli --channel 6.0
     .cli/dotnet-install.sh -i .cli --channel 7.0
+    .cli/dotnet-install.sh -i .cli --channel 8.0
   fi
 
   # Display info
